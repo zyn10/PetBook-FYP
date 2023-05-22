9ae1794a961ab218519040ce93ab7cd12eb75828
Muhammad Zain <zainandcs@gmail.com>
2023-05-22 21:57:04 +0500

    plugins

--- Dev/.dart_tool/dartpad/web_plugin_registrant.dart
+++ Dev/.dart_tool/dartpad/web_plugin_registrant.dart
@@ -10,7 +10,9 @@ import 'package:cloud_firestore_web/cloud_firestore_web.dart';
 import 'package:firebase_auth_web/firebase_auth_web.dart';
 import 'package:firebase_core_web/firebase_core_web.dart';
 import 'package:firebase_storage_web/firebase_storage_web.dart';
+import 'package:geolocator_web/geolocator_web.dart';
 import 'package:image_picker_for_web/image_picker_for_web.dart';
+import 'package:location_web/location_web.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
@@ -20,7 +22,9 @@ void registerPlugins([final Registrar? pluginRegistrar]) {
   FirebaseAuthWeb.registerWith(registrar);
   FirebaseCoreWeb.registerWith(registrar);
   FirebaseStorageWeb.registerWith(registrar);
+  GeolocatorPlugin.registerWith(registrar);
   ImagePickerPlugin.registerWith(registrar);
+  LocationWebPlugin.registerWith(registrar);
   SharedPreferencesPlugin.registerWith(registrar);
   registrar.registerMessageHandler();
 }
