3ec3ed98e38b387149257ab747fffe5b00800bc3
Muhammad Zain <zainandcs@gmail.com>
2023-05-22 21:55:26 +0500

    mac replace

--- Dev/macos/Flutter/GeneratedPluginRegistrant.swift
+++ Dev/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,6 +9,8 @@ import cloud_firestore
 import firebase_auth
 import firebase_core
 import firebase_storage
+import geolocator_apple
+import location
 import path_provider_foundation
 import shared_preferences_foundation
 
@@ -17,6 +19,8 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
