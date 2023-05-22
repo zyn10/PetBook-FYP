8326a0e0de5e09138ae63079974f8b254f49aa11
Muhammad Zain <zainandcs@gmail.com>
2023-05-22 21:54:42 +0500

    two

--- Dev/android/app/src/main/kotlin/com/example/petbook/MainActivity.kt
+++ Dev/android/app/src/main/kotlin/com/example/petbook/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.example.petbook
 
 import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
+import io.flutter.embedding.android.FlutterFragmentActivity
+class MainActivity: FlutterFragmentActivity() {
 }
